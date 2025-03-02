5b62c2139e75cda8269b98a2c2c0082076ec87e2
Eduardo Tavares <eduardo.tavares002@edu.pucrs.br>
2025-03-02 11:54:43 -0300

    Add Keeper App #1

---
+++ React/Keeper-App/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Note from './components/Note/Note'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Note/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
